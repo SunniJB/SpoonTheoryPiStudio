7937e7b57d6d141d61c0baaae93c85ca4c2b1429
loldani123 <harestaddaniel@gmail.com>
2022-09-23 16:00:23 +0200

    Updated memory minigame
    has timer, gives work performance and money. fixed a bug.

--- Spoon Theory PiStudios/Assets/Scripts/Minigames/memory game/ClickMemory.cs
+++ Spoon Theory PiStudios/Assets/Scripts/Minigames/memory game/ClickMemory.cs
@@ -8,46 +8,51 @@ public class ClickMemory : MonoBehaviour
 {
     public Sprite[] images = new Sprite[2];
     private int arrayPos = 1;
+    private bool canClick = true;
 
     public void OnClick()
     {
-        if (arrayPos == 1)
+        if (canClick)
         {
-            GetComponent<Image>().overrideSprite = images[arrayPos];
-
             if (arrayPos == 1)
             {
-                arrayPos = 0;
-            }
-            else
-            {
-                arrayPos = 1;
-            }
+                GetComponent<Image>().overrideSprite = images[arrayPos];
 
-            if (MemoryManager.instance.clickOne == null)
-            {
-                MemoryManager.instance.clickOne = images[1];
-                MemoryManager.instance.firstClickObj = this.gameObject;
-            }
-            else
-            {
-                MemoryManager.instance.clickTwo = images[1];
-                if (MemoryManager.instance.clickOne == MemoryManager.instance.clickTwo)
+                if (arrayPos == 1)
                 {
-                    Debug.Log("You Win Lmao"); // wincon!!!!!
-                    MemoryManager.instance.clickOne = null;
-                    MemoryManager.instance.clickTwo = null;
-                    MemoryManager.instance.wins++;
+                    arrayPos = 0;
                 }
                 else
                 {
-                    Invoke("Fail", 0.5f);
+                    arrayPos = 1;
+                }
+
+                if (MemoryManager.instance.clickOne == null)
+                {
+                    MemoryManager.instance.clickOne = images[1];
+                    MemoryManager.instance.firstClickObj = this.gameObject;
+                }
+                else
+                {
+                    MemoryManager.instance.clickTwo = images[1];
+                    if (MemoryManager.instance.clickOne == MemoryManager.instance.clickTwo)
+                    {
+                        Debug.Log("You Win Lmao"); // wincon!!!!!
+                        MemoryManager.instance.clickOne = null;
+                        MemoryManager.instance.clickTwo = null;
+                        MemoryManager.instance.wins++;
+                    }
+                    else
+                    {
+                        Invoke("Fail", 0.5f);
+                        canClick = false;
+                    }
                 }
             }
-        }
-        else
-        {
-            Debug.Log("You can't flip it back dumbo!");
+            else
+            {
+                Debug.Log("You can't flip it back dumbo!");
+            }
         }
     }
 
@@ -60,5 +65,6 @@ public class ClickMemory : MonoBehaviour
         MemoryManager.instance.firstClickObj.GetComponent<Image>().overrideSprite = images[0];
         arrayPos = 1;
         MemoryManager.instance.firstClickObj.GetComponent<ClickMemory>().arrayPos = 1;
+        canClick = true;
     }
 }
