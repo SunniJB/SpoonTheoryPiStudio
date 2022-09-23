9bcf46bcb6691b37a007ae5732ce359125f3c1d8
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2022-09-23 16:20:38 +0200

    simon says

--- Spoon Theory PiStudios/Assets/Scripts/Minigames/Simonsays game/SimonSaysManager.cs
+++ Spoon Theory PiStudios/Assets/Scripts/Minigames/Simonsays game/SimonSaysManager.cs
@@ -5,76 +5,50 @@ using UnityEngine.UI;
 
 public class SimonSaysManager : MonoBehaviour
 {
-    int value, count, currentLength;
+    int value, count, currentLength, maxStrikes, currentStrikes, timesSequencePlayed;
 
     List<int> currentSequence = new List<int>();
 
     [SerializeField] int initialLength, repeatTimes;
 
-    [SerializeField] float initialTimeBetweenColors;
+    [SerializeField] float initialTimeBetweenColors, resetColorTime, beginSequenceDelay;
     float currentTimeBetweenColors;
 
-    [SerializeField] Image red, green, blue, yellow;
+    [SerializeField] Image red, green, blue, yellow, winPanel, losePanel;
+    [SerializeField] Image[] strikes;
+
+    [SerializeField] Color blueCol, greenCol, yellowCol, redCol, whiteCol;
+    
     // Start is called before the first frame update
     void Start()
     {
         count = 0;
+        currentStrikes = 0;
+        timesSequencePlayed = 0;
+        maxStrikes = 3;
         currentLength = initialLength;
         currentTimeBetweenColors = initialTimeBetweenColors;
+
+        losePanel.gameObject.SetActive(false);
+        winPanel.gameObject.SetActive(false);
+
+        DeactivateClick();
+
+        CreateSequence();
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-    }
-
-    public void RedClick()
-    {
-        Debug.Log("red");
-        value = 0;
-        count++;
-    }
-
-    public void YellowClick()
-    {
-        Debug.Log("yellow");
-        value = 1;
-        count++;
-    }
-
-    public void BlueClick()
-    {
-        Debug.Log("blue");
-        value = 2;
-        count++;
-    }
-    public void GreenClick()
-    {
-        Debug.Log("green");
-        value = 3;
-        count++;
-    }
-
-    void CreateSequence()
-    {
-        for (int i = 0; i < currentLength; i++)
+        if (count > 0 && count >= currentSequence.Count)
         {
-            value = Random.Range(0, 4);
-            currentSequence.Add(value);
+            if (timesSequencePlayed < repeatTimes) 
+                NextSequence();
+            else Win();
         }
-        currentLength++;
     }
 
-    void ResetSequence()
-    {
-        currentSequence.Clear();
-    }
-
-    IEnumerator ShowSequence()
-    {
-        yield return new WaitForSeconds(currentTimeBetweenColors);
-    }
+    #region Click
 
     void DeactivateClick()
     {
@@ -90,5 +64,134 @@ public class SimonSaysManager : MonoBehaviour
         yellow.raycastTarget = true;
         blue.raycastTarget = true;
         green.raycastTarget = true;
+    }
+
+    public void RedClick()
+    {
+        Debug.Log("red");
+        value = 0;
+        ChangeColor(red, redCol);
+        CheckIfCorrect(value);
+        count++;
+    }
+
+    public void YellowClick()
+    {
+        Debug.Log("yellow");
+        value = 1;
+        ChangeColor(yellow, yellowCol);
+        CheckIfCorrect(value);
+        count++;
+    }
+
+    public void BlueClick()
+    {
+        Debug.Log("blue");
+        value = 2;
+        ChangeColor(blue, blueCol);
+        CheckIfCorrect(value);
+        count++;
+    }
+    public void GreenClick()
+    {
+        Debug.Log("green");
+        value = 3;
+        ChangeColor(green, greenCol);
+        CheckIfCorrect(value);
+        count++;
+    }
+
+    void ChangeColor(Image img, Color color)
+    {
+        img.color = color;
+        StartCoroutine(ResetColor(img));
+    }
+
+    IEnumerator ResetColor(Image img)
+    {
+        yield return new WaitForSeconds(resetColorTime);
+        img.color = whiteCol;
+    }
+
+    #endregion
+
+    void CreateSequence()
+    {
+        for (int i = 0; i < currentLength; i++)
+        {
+            value = Random.Range(0, 4);
+            currentSequence.Add(value);
+        }
+        currentLength++;
+
+        StartCoroutine(ShowSequence());
+    }
+
+    void ResetSequence()
+    {
+        timesSequencePlayed++;
+        count = 0;
+        currentSequence.Clear();
+    }
+
+    IEnumerator ShowSequence()
+    {
+        yield return new WaitForSeconds(beginSequenceDelay);
+        int i = 0;
+        while (i < currentSequence.Count)
+        {
+            switch (currentSequence[i])
+            {
+                case 0:
+                    ChangeColor(red, redCol);
+                    break;
+
+                case 1:
+                    ChangeColor(yellow, yellowCol);
+                    break;
+
+                case 2:
+                    ChangeColor(blue, blueCol);
+                    break;
+
+                case 3:
+                    ChangeColor(green, greenCol);
+                    break;
+            }
+            i++;
+            yield return new WaitForSeconds(currentTimeBetweenColors);
+        }
+    }
+
+    void NextSequence()
+    {
+        DeactivateClick();
+
+        currentTimeBetweenColors -= 0.1f;
+
+        ResetSequence();
+        CreateSequence();
+    }
+
+    void CheckIfCorrect(int value)
+    {
+        if (value != currentSequence[count])
+        {
+            currentStrikes++;
+            strikes[currentStrikes - 1].gameObject.SetActive(true);
+            NextSequence();
+        }
+
+        if (currentStrikes >= maxStrikes) Lose();
+    }
+
+    void Lose()
+    {
+        losePanel.gameObject.SetActive(true);
+    }
+
+    void Win()
+    {
+        winPanel.gameObject.SetActive(true);
     }
 }
