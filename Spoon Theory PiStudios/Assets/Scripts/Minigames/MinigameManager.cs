befd9a9ebe1b5e78512cd5ba6d76af61496946c8
loldani123 <harestaddaniel@gmail.com>
2022-10-06 15:13:45 +0200

    changed minigamemanager and implemented it in memorymanager
    minigamemanager now allows you to get the values of workperformance and money so you can show them in the win screen of the minigame. you can also change the dividend of the work performance calculation

--- Spoon Theory PiStudios/Assets/Scripts/Minigames/MinigameManager.cs
+++ Spoon Theory PiStudios/Assets/Scripts/Minigames/MinigameManager.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class MinigameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float workPerform;
+    float money;
 
-    // Update is called once per frame
-    void Update()
+    public void Complete(int _spoonCost, float _totalTime, float _dividend = 100)
     {
-        
-    }
-
-    public void Complete(int _spoonCost, float _totalTime)
-    {
-        float workPerform = (100 / (_totalTime / 10)) - 5 + GameManager.GetInstance().happiness;
+        workPerform = (_dividend / (_totalTime / 10)) - 5 + GameManager.GetInstance().happiness;
         if (workPerform > 50) workPerform = 50;
-        float money = 10.9f * (workPerform / 10);
+        money = 10.9f * (workPerform / 10);
         GameManager.GetInstance().money += money;
         GameManager.GetInstance().spoons -= _spoonCost;
     }
@@ -28,5 +19,15 @@ public class MinigameManager : MonoBehaviour
     public void Skip()
     {
         Complete(Random.Range(2, 4), Random.Range(10, 35));
+    }
+
+    public float GetWorkPerform()
+    {
+        return workPerform;
+    }
+
+    public float GetMoney()
+    {
+        return money;
     }
 }
