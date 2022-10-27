7df41062c9910760d2739efc913497ae732bebb2
thejkr333 <80771269+thejkr333@users.noreply.github.com>
2022-10-27 14:11:07 +0200

    fixed level manager

--- Spoon Theory PiStudios/Assets/Scripts/Managers/LevelManager.cs
+++ Spoon Theory PiStudios/Assets/Scripts/Managers/LevelManager.cs
@@ -13,9 +13,6 @@ public class LevelManager : MonoBehaviour
 
     [SerializeField] string audioName;
 
-    public static LevelManager instance;
-    public static LevelManager GetInstance() { return instance; }
-
     public GameObject wakeUpButton;
     // Start is called before the first frame update
     void Start()
