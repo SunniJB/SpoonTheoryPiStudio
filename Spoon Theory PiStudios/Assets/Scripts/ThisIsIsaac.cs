23569fe066a471ff3d8b3641d6eda55b73de3220
SunniJB <40769150+SunniJB@users.noreply.github.com>
2022-11-11 10:39:36 +0100

    The right avatars

--- Spoon Theory PiStudios/Assets/Scripts/ThisIsIsaac.cs
+++ Spoon Theory PiStudios/Assets/Scripts/ThisIsIsaac.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ThisIsIsaac : MonoBehaviour
 {
+
+    [SerializeField] Image avatar;
+    [SerializeField] Sprite fullSpoonsAvatar;
+    [SerializeField] Sprite highSpoonsAvatar;
+    [SerializeField] Sprite halfSpoonsAvatar;
+    [SerializeField] Sprite lowSpoonsAvatar;
+    [SerializeField] CharacterInteractor characterInteractor;
+
     private void Awake()
     {
         BecomeIsaac();
@@ -10,5 +19,9 @@ public class ThisIsIsaac : MonoBehaviour
     public void BecomeIsaac()
     {
         GameManager.GetInstance().isIsaac = true;
+        characterInteractor.fullSpoonsAvatar = fullSpoonsAvatar;
+        characterInteractor.highSpoonsAvatar = highSpoonsAvatar;
+        characterInteractor.halfSpoonsAvatar = halfSpoonsAvatar;
+        characterInteractor.lowSpoonsAvatar = lowSpoonsAvatar;
     }
 }
